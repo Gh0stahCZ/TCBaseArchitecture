0107c351b2d4b712e7c312ea6d28be1daa945997
Tomáš Chlápek <contact@tomaschlapek.com>
2017-08-22 17:47:54 +0200

    Sample job scheduler added.

--- app/src/main/java/com/tomaschlapek/tcbasearchitecture/helper/NetworkHelper.java
+++ app/src/main/java/com/tomaschlapek/tcbasearchitecture/helper/NetworkHelper.java
@@ -1,8 +1,17 @@
 package com.tomaschlapek.tcbasearchitecture.helper;
 
 import android.content.Context;
+import android.os.Bundle;
 
+import com.firebase.jobdispatcher.Constraint;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.RetryStrategy;
+import com.firebase.jobdispatcher.Trigger;
 import com.squareup.picasso.Picasso;
+import com.tomaschlapek.tcbasearchitecture.presentation.ui.service.AutoUpdateJobService;
 
 import retrofit2.Retrofit;
 
@@ -11,21 +20,28 @@ import retrofit2.Retrofit;
  */
 public class NetworkHelper {
 
+  /* Private Constants ****************************************************************************/
+  /* Private Attributes ***************************************************************************/
+
   /**
    * Application context.
    */
   private Context mContext;
 
   /**
+   * Dispatcher for scheduling network jobs.
+   */
+  private FirebaseJobDispatcher mDispatcher;
+
+  /**
    * Retrofit instance.
    */
-  Retrofit mRetrofit;
+  private Retrofit mRetrofit;
 
   /**
    * Picasso instance
    */
-  Picasso mPicasso;
-
+  private Picasso mPicasso;
 
   /* Constructor **********************************************************************************/
 
@@ -33,12 +49,12 @@ public class NetworkHelper {
     mContext = context;
     mRetrofit = retrofit;
     mPicasso = picasso;
+    mDispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(context));
     init();
   }
 
-  private void init() {
-    // TODO
-  }
+  /* Public Static Methods ************************************************************************/
+  /* Public Methods *******************************************************************************/
 
   /**
    * Dummy method.
@@ -57,4 +73,65 @@ public class NetworkHelper {
   public Picasso getPicasso() {
     return mPicasso;
   }
+
+  public void scheduleAutoUpdateJob() {
+    Job autoUpdateJob = mDispatcher.newJobBuilder()
+      .setService(AutoUpdateJobService.class) // the JobService that will be called
+      .setTag(AutoUpdateJobService.AUTO_UPDATE_JOB_TAG)        // uniquely identifies the job
+      .build();
+
+    mDispatcher.mustSchedule(autoUpdateJob);
+  }
+
+  public void scheduleAutoUpdateComplexJob() {
+    Bundle myExtrasBundle = new Bundle();
+    myExtrasBundle.putString("some_key", "some_value");
+
+    Job myJob = mDispatcher.newJobBuilder()
+      // the JobService that will be called
+      .setService(AutoUpdateJobService.class)
+      // uniquely identifies the job
+      .setTag(AutoUpdateJobService.AUTO_UPDATE_JOB_TAG)
+      // one-off job
+      .setRecurring(false)
+      // don't persist past a device reboot
+      .setLifetime(Lifetime.UNTIL_NEXT_BOOT)
+      // start between 0 and 60 seconds from now
+      .setTrigger(Trigger.executionWindow(0, 60))
+      // don't overwrite an existing job with the same tag
+      .setReplaceCurrent(false)
+      // retry with exponential backoff
+      .setRetryStrategy(RetryStrategy.DEFAULT_EXPONENTIAL)
+      // constraints that need to be satisfied for the job to run
+      .setConstraints(
+        // only run on an unmetered network
+        Constraint.ON_UNMETERED_NETWORK,
+        // only run when the device is charging
+        Constraint.DEVICE_CHARGING
+      )
+      .setExtras(myExtrasBundle)
+      .build();
+
+    mDispatcher.mustSchedule(myJob);
+  }
+
+  public void cancelAutoUpdateJob() {
+    mDispatcher.cancel(AutoUpdateJobService.AUTO_UPDATE_JOB_TAG);
+  }
+
+  public void cancelAllJobs() {
+    mDispatcher.cancelAll();
+  }
+
+  /* Private Methods ******************************************************************************/
+
+  private void init() {
+    // TODO
+  }
+
+  /* Getters / Setters ****************************************************************************/
+  /* Inner classes ********************************************************************************/
+
+
+
 }
